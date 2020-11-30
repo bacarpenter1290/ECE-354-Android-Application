eb15c509d2f9bcf98197bec0b2a49b7f96908205
Baylee Carpenter <bayleecarpenter1999@gmail.com>
2020-11-30 01:05:30 -0600

    Inital Upload

---
+++ CoconinoOnlineShoppingApplication/app/src/main/java/com/example/coconinoonlineshoppingapplication/ProductLine.java
@@ -0,0 +1,44 @@
+package com.example.coconinoonlineshoppingapplication;
+
+public class ProductLine {
+	private int productLineNumber;
+	private String productLine;
+	private String description;
+	
+	public ProductLine(int productLineNumber, String productLine, String description) {
+		this.productLineNumber = productLineNumber;
+		this.productLine = productLine;
+		this.description = description;
+	}
+	
+	public ProductLine() {
+		this.productLineNumber = 0;
+		this.productLine = "";
+		this.description = "";
+	}
+	
+	@Override
+	public String toString() {
+		return productLine;
+	}
+	
+	public int getProductLineNumber() {
+		return productLineNumber;
+	}
+	public void setProductLineNumber(int productLineNumber) {
+		this.productLineNumber = productLineNumber;
+	}
+	public String getProductLine() {
+		return productLine;
+	}
+	public void setProductLine(String productLine) {
+		this.productLine = productLine;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
---
+++ CoconinoOnlineShoppingApplication/app/src/main/java/com/example/coconinoonlineshoppingapplication/ShoppingCartDetail.java
@@ -0,0 +1,53 @@
+package com.example.coconinoonlineshoppingapplication;
+
+public class ShoppingCartDetail {
+	private int shoppingCartNumber;
+	private int productNumber;
+	private Product product;
+	private int quantity;
+	
+	public ShoppingCartDetail(int shoppingCartNumber, int productNumber, int quantity) {
+		this.shoppingCartNumber = shoppingCartNumber;
+		this.productNumber = productNumber;
+		this.quantity = quantity;
+	}
+	
+	public ShoppingCartDetail() {
+		this.shoppingCartNumber = 0;
+		this.productNumber = 0;
+		this.quantity = 0;
+	}
+	
+	@Override
+	public String toString() {
+		return "ShoppingCartDetail [shoppingCartNumber=" + shoppingCartNumber + ", productNumber=" + productNumber
+				+ ", product=" + product + ", quantity=" + quantity + "]";
+	}
+	
+	public int getShoppingCartNumber() {
+		return shoppingCartNumber;
+	}
+	public void setShoppingCartNumber(int shoppingCartNumber) {
+		this.shoppingCartNumber = shoppingCartNumber;
+	}
+	public int getProductNumber() {
+		return productNumber;
+	}
+	public void setProductNumber(int productNumber) {
+		this.productNumber = productNumber;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
